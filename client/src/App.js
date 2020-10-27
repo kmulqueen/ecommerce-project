e486b0f99da71289fb3f8f93f67d8cd5ab95782d
Kyle Mulqueen <kmulqdrums710@gmail.com>
2020-10-27 12:32:40 -0600

    Set up react-router-dom. Created product page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomePage />
+          <Route path="/" exact component={HomePage} />
+          <Route path="/product/:id" component={ProductPage} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -1,20 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Rating from "../Rating";
 import { Card } from "react-bootstrap";
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
-      </a>
+      </Link>
 
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as="div">
           <Rating rating={product.rating} reviews={product.numReviews} />
         </Card.Text>
