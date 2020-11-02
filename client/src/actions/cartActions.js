e642d9cccd461882827e50f220f532bbb6a8955d
Kyle Mulqueen <kmulqdrums710@gmail.com>
2020-11-02 10:10:13 -0700

    Added remove from cart functionality. Created remove from cart action and implemented in cart page.

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CART_ADD_ITEM } from "../action_types/cartTypes";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../action_types/cartTypes";
 
 export const addToCart = (id, quantity) => async (dispatch, getState) => {
   const res = await axios.get(`/api/products/${id}`);
@@ -14,6 +14,15 @@ export const addToCart = (id, quantity) => async (dispatch, getState) => {
       numInStock: res.data.numInStock,
       quantity,
     },
+  });
+
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => async (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
   });
 
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
--- client/src/reducers/cartReducers.js
+++ client/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from "../action_types/cartTypes";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../action_types/cartTypes";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
@@ -22,6 +22,11 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           cartItems: [...state.cartItems, item],
         };
       }
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+      };
     default:
       return state;
   }
