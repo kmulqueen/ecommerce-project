746b38412adeedac542ada99b92801dfbef4dca6
Kyle Mulqueen <kmulqdrums710@gmail.com>
2020-11-02 10:01:14 -0700

    Added cart items to cart page. Logic for calculating subtotal.

--- client/src/pages/CartPage/CartPage.js
+++ client/src/pages/CartPage/CartPage.js
@@ -31,7 +31,98 @@ const CartPage = ({ match, location, history }) => {
     }
   }, [dispatch, productID, productQuantity]);
 
-  return <div>Cart Page</div>;
+  const removeFromCartHandler = (id) => {
+    console.log("remove");
+  };
+
+  const checkoutHandler = () => {
+    // If customer is not logged in, push to login screen. Otherwise redirect to shipping
+    history.push("/login?redirect=shipping");
+  };
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <>
+            <Message>Your cart is empty.</Message>
+            <Link to="/" className="btn btn-dark my-3">
+              Go Back
+            </Link>
+          </>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as="select"
+                      value={item.quantity}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.numInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className="fas fa-trash"></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>
+                Subtotal (
+                {cartItems.reduce((acc, item) => acc + item.quantity, 0)}) items
+              </h3>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.quantity * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type="button"
+                className="btn-block"
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              >
+                Proceed to Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default CartPage;
