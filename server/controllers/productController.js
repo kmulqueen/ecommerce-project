63c627233e1831b9ea361268fea18ede38a7a146
Kyle Mulqueen <kmulqdrums710@gmail.com>
2020-11-22 17:31:11 -0700

    Created routes for admin to create and update products.

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -26,4 +26,57 @@ module.exports = {
       res.status(404).json({ message: "Product not found." });
     }
   },
+  createProduct: async function (req, res) {
+    try {
+      // @TODO - DELETE SAMPLE INFO - Filled with sample info for now. WILL DELETE SAMPLE INFO
+      const product = new db.Product({
+        name: "Sample name",
+        price: 0,
+        user: req.user._id,
+        image: "/images/sample.jpg",
+        brand: "Sample brand",
+        category: "Sample category",
+        numInStock: 0,
+        numReviews: 0,
+        description: "Sample description",
+      });
+
+      const createdProduct = await product.save();
+      res.status(201).json(createdProduct);
+    } catch (error) {
+      res.status(422).json(error);
+    }
+  },
+  updateProduct: async function (req, res) {
+    try {
+      const {
+        name,
+        price,
+        description,
+        image,
+        brand,
+        category,
+        numInStock,
+      } = req.body;
+
+      const product = await db.Product.findById(req.params.id);
+
+      if (product) {
+        product.name = name;
+        product.price = price;
+        product.description = description;
+        product.image = image;
+        product.brand = brand;
+        product.category = category;
+        product.numInStock = numInStock;
+
+        const updatedProduct = await product.save();
+        res.json(updatedProduct);
+      } else {
+        res.status(404).json({ message: "Product not found." });
+      }
+    } catch (error) {
+      res.status(422).json(error);
+    }
+  },
 };
--- server/routes/api/productRoutes.js
+++ server/routes/api/productRoutes.js
@@ -4,9 +4,11 @@ const { admin, protect } = require("../../middleware/authMiddleware");
 
 // Matches with "/api/products/"
 router.route("/").get(productController.findAll);
+router.route("/").post(protect, admin, productController.createProduct);
 
 // Matches with "/api/products/:id"
 router.route("/:id").get(productController.findById);
+router.route("/:id").put(protect, admin, productController.updateProduct);
 router.route("/:id").delete(protect, admin, productController.deleteById);
 
 module.exports = router;
